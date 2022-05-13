557a51e0ffaad8a84f48b3b289c3c94a4b5864d7
Julian Toscani <julian.toscani@rtl.com>
2022-05-13 09:09:03 +0200

    🚧  create html from md-files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-import fs from "fs/promises";
-import path from "path";
+import * as fs from "fs/promises";
+import * as path from "path";
+import { marked } from "marked";
+import createDir from "./helper/createDir";
 
-fs.readFile(path.join(__dirname, "../content/content.md")).then((content) => {
-    console.log(content.toString());
-});
+const getContentPath = (filename: string) =>
+  path.join(__dirname, "../content/", filename);
+const getOutputPath = (filename: string) =>
+  path.join(__dirname, "../dist/", filename);
+
+fs.readFile(getContentPath("content.md"), "utf-8").then(
+  async (content: string) => {
+    const markedContent = marked.parse(content);
+    const dirPath = path.join(__dirname, "../dist/");
+
+    try {
+      await createDir(dirPath)
+      fs.writeFile(getOutputPath("content.html"), markedContent);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
